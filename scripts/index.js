a3a5540b590cc20f31106c3ed0568ca10f5f2848
WitFelm <jacobwitflem@gmail.com>
2021-01-05 15:40:34 +0200

    theme toggle

--- scripts/index.js
+++ scripts/index.js
@@ -1,5 +1,19 @@
-function main() {
-	image = document.getElementsByClassName("")
+function domcofiguration() {
+	document.getElementById('themeswitch').onclick = function() {
+		console.log("nerd")
+		console.log("theme change");
+		theme = document.getElementById('theme');
+		console.log(theme)
+		console.log(theme.href)
+		console.log(theme.href == "css/style.css")
+		if (theme.href == "file:///home/witfelm/Documents/Programming/climbing/css/style.css") {
+			theme.href = "file:///home/witfelm/Documents/Programming/climbing/css/dark.css";
+		} else {
+			theme.href = "file:///home/witfelm/Documents/Programming/climbing/css/style.css";
+		}
+	}
 }
 
-main()
+window.addEventListener("DOMContentLoaded", function(){
+	domcofiguration()
+});
